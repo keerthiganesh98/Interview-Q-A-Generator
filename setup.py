5ab0d731f8e7349cc3c4065b3b7e1a35751940c3
keerthiganesh98 <keerthimuru@gmail.com>
2024-10-07 20:32:31 +0530

    new folder strucutre setup for project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Generative AI Project",
+    version= '0.0.0',
+    author="keerthi ganesh",
+    author_email="keerthi****@gmail..com",
+    packages= find_packages(),
+    install_requires = []
+)
